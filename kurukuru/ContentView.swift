1b07a71081e57392a0cdd90435f6ab0a26f343ea
sumi <ninzin60en.dev@gmail.com>
2021-09-20 16:55:17 +0900

    くるくるの位置を修正

--- kurukuru/ContentView.swift
+++ kurukuru/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var viewModel: ViewModel = ViewModel()
     var body: some View {
-        VStack{
+        GeometryReader{ geometry in
             Button(action: {
                 viewModel.changeKurukuruViewState()
             }) {
                 Text("くるくる")
             }
-            .padding()
+            .frame(width: geometry.size.width, height: geometry.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
             if viewModel.kurukuruFrg {
                 ProgressView("Now Loading")
-                    .scaleEffect(x: 2, y: 2, anchor: .bottom)
-            }
+                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)            }
         }
     }
 }
